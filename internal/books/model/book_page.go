0773c8305b25373d810ee38f39461e4a6879a923
bellapacx <bellapacx@gmail.com>
2026-07-08 10:06:42 +0300

    8 commitsddsdsdssdsssffs

--- internal/books/model/book_page.go
+++ internal/books/model/book_page.go
@@ -4,4 +4,5 @@ type BookPage struct {
 	PageNumber int    `json:"page_number"`
 	Content    string `json:"content"`
 	ImageURL   string `json:"image_url"`
+	AudioURL   *string `json:"audio_url,omitempty"`
 }
